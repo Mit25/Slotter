dec2d8739ee8ad6924564541ee426a5c2dbfaaa3
niraj1997 <nirajvadhaiya@gmail.com>
2018-02-17 15:25:58 +0530

    complete

--- app/src/main/java/com/example/android/slotter/addEvent.java
+++ app/src/main/java/com/example/android/slotter/addEvent.java
@@ -1,9 +1,12 @@
 package com.example.android.slotter;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +15,8 @@ import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,8 +25,9 @@ import java.util.Calendar;
 
 public class addEvent extends AppCompatActivity {
 
-    private TextView mDisplaystartDate,mDisplayendDate;
+    private TextView mDisplaystartDate,mDisplayendDate,mDisplaystatTime,mDisplayendTime;
     private DatePickerDialog.OnDateSetListener mDateSetListener,mDateSetListener1;
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener,mTimeSetListener1;
     DatabaseReference databaseevent;
 
 
@@ -33,8 +39,10 @@ public class addEvent extends AppCompatActivity {
         databaseevent = FirebaseDatabase.getInstance().getReference();
 
         mDisplaystartDate = (TextView) findViewById(R.id.startDate);
-
         mDisplayendDate = (TextView) findViewById(R.id.endDate);
+
+        mDisplaystatTime = (TextView) findViewById(R.id.stime);
+        mDisplayendTime = (TextView) findViewById(R.id.etime);
 
         mDisplaystartDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,22 +100,110 @@ public class addEvent extends AppCompatActivity {
             }
         };
 
+        mDisplaystatTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(addEvent.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        mDisplaystatTime.setText( selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
 
+            }
+        });
+
+        mDisplayendTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(addEvent.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        mDisplayendTime.setText( selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+
+            }
+        });
+    }
+
+    public boolean slotCheccker(String sTime,String eTime,String noSlot,String interval,String slotDu)
+    {
+        String stime[] = sTime.split(":");
+        String etime[] = eTime.split(":");
+        int slot = Integer.parseInt(noSlot);
+        int inter = Integer.parseInt(interval);
+        int duration = Integer.parseInt(slotDu);
+
+        int h = Integer.parseInt(etime[0])-Integer.parseInt(stime[0]);
+        int m = Integer.parseInt(etime[1])-Integer.parseInt(stime[1]);
+
+        int total = h+m;
+        int need = inter*(slot-1)+slot*duration;
+        if(total<need)
+            return false;
+
+        return true;
     }
 
     public void addEvent1(View v)
     {
         EditText eventName = (EditText) findViewById(R.id.ename);
-        EditText sdate = (EditText) findViewById(R.id.startDate);
-        EditText edate = (EditText) findViewById(R.id.endDate);
+        TextView sdate = (TextView) findViewById(R.id.startDate);
+        TextView edate = (TextView) findViewById(R.id.endDate);
         EditText edescription = (EditText) findViewById(R.id.eventDescription);
         EditText ecode = (EditText) findViewById(R.id.ecode);
-        
+        EditText noSlot = (EditText) findViewById(R.id.noSlot);
+        EditText slotDu = (EditText) findViewById(R.id.slotdur);
+        EditText interval = (EditText) findViewById(R.id.slotint);
+        TextView sTime = (TextView) findViewById(R.id.stime);
+        TextView eTime = (TextView) findViewById(R.id.etime);
+        /*sdate.setPaintFlags(sdate.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        edate.setPaintFlags(edate.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        sTime.setPaintFlags(sTime.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        eTime.setPaintFlags(eTime.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);*/
 
         SharedPreferences sp = getSharedPreferences("key", Context.MODE_PRIVATE);
         String uname = sp.getString("value" , null);
 
         Log.d("uname",uname);
+
+        if(eventName.getText().toString().length()==0 || sdate.getText().toString().length()==0|| edate.getText().toString().length()==0 || edescription.getText().toString().length()==0
+                || ecode.getText().toString().length()==0 || noSlot.getText().toString().length()==0 || slotDu.getText().toString().length()==0 || interval.getText().toString().length()==0
+        || sTime.getText().toString().length()==0 || eTime.getText().toString().length() == 0)
+        {
+            Toast.makeText(getApplicationContext(),"Retry",Toast.LENGTH_SHORT).show();
+            noSlot.getText().clear();
+            slotDu.getText().clear();
+            interval.getText().clear();
+            return;
+        }
+        else
+        {
+            if(!slotCheccker(sTime.getText().toString(),eTime.getText().toString(),noSlot.getText().toString(),interval.getText().toString(),slotDu.getText().toString()))
+            {
+                Toast.makeText(getApplicationContext(),"Reenter No. of slot",Toast.LENGTH_SHORT).show();
+            }
+            else{
+            Toast.makeText(getApplicationContext(),"Event Added Successfully",Toast.LENGTH_SHORT).show();
+            Intent goToNextActivity = new Intent(getApplicationContext(), DashboardActivity.class);
+            startActivity(goToNextActivity);
+            }
+        }
 
         Event e = new Event(eventName.getText().toString(),sdate.getText().toString(),edate.getText().toString(),edescription.getText().toString(),ecode.getText().toString(),uname);
 
